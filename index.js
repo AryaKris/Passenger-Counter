2da065de40d569c9b01de7241611700d55db815c
Arya Krishna <dr.aryakris@outlook.com>
2022-08-01 23:29:15 -0700

    increment button and function

---
+++ index.js
@@ -0,0 +1,15 @@
+
+//initialize the count as zero
+//listen for the clicks on the increment button
+//increment the count variable when the button is clicked
+//change the count-el in the HTML to reflect the new count 
+
+
+
+
+function increment (){
+    
+}
+
+
+
