076908d63ce41d3d17da5e1c813b0b5bccd553a1
Arya Krishna <dr.aryakris@outlook.com>
2022-08-01 23:53:44 -0700

    working with DOM

--- index.js
+++ index.js
@@ -4,11 +4,24 @@
 //increment the count variable when the button is clicked
 //change the count-el in the HTML to reflect the new count 
 
+//LET variables are block scoped !!
+//Working with DOM - Document Object Model ( how you use JS to modify the website)
 
+let CountEl = document.getElementById("count-el") //pass in arguments
+console.log(CountEl)
 
-
+//grab the count-el element , store it in a countEl variable
+let count = 0
 function increment (){
+
+   count = count+1
+   //set countEl's innertext to the count
+   CountEl.innerText = count
+
+   console.log(count)
     
+
+
 }
 
 
